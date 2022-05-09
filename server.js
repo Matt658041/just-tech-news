e3f94414da017d792115e38cd6aba2f9b8f14115
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-05-09 09:15:10 -0500

    corrected css link

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 const path = require('path');
 const express = require ('express');
 const routes = require('./routes');
@@ -5,6 +7,10 @@ const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 
 app.use (express.json());
 app.use(express.urlencoded({extended: true}));
